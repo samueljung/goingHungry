ff758ee10a480d6f8eb0f54c73ed72c953cda71c
Julie Wang <julie.wang@emory.edu>
2019-09-28 19:17:17 -0400

    fixed the html links

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var clubsRouter = require("./routes/clubs")
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/clubs', clubsRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
